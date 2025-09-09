1db58cbde60396fd96cc2c7416a181f155647271
nikita <nikit15110295@gmail.com>
2025-09-09 15:05:07 +0700

    EDIT. Кнопка для раскрытия/сворачивания карточек категорий

--- src/app/core/components/category-section/category-section.component.ts
+++ src/app/core/components/category-section/category-section.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-category-section',
@@ -8,9 +8,38 @@ import { RouterLink } from '@angular/router';
   templateUrl: './category-section.component.html',
   styleUrl: './category-section.component.scss'
 })
-export class CategorySectionComponent {
+export class CategorySectionComponent implements OnChanges {
+  
+  @Input() categories: any[] = [];  // входящий массив категорий
+  displayedCategories: any[] = [];   // категории, которые будут отображаться
+  showAllCategories: boolean = false;  // флаг для кнопки "Еще"
+  isMainPage: boolean = false;  // флаг для проверки, на главной ли мы странице
 
-  @Input() categories: any;
+  constructor(private router: Router) {}
 
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['categories']) {
+      this.updateDisplayedCategories();
+    }
+  }
+
+  ngOnInit() {
+    this.isMainPage = this.router.url === '/';
+    this.updateDisplayedCategories();
+  }
+
+  updateDisplayedCategories() {
+    if (this.isMainPage) {
+      // На главной странице показываем только 2 строки по 5 категорий (10 карточек)
+      this.displayedCategories = this.categories.slice(0, this.showAllCategories ? this.categories.length : 10);
+    } else {
+      // Для других страниц показываем все категории
+      this.displayedCategories = this.categories;
+    }
+  }
+
+  toggleDisplay() {
+    this.showAllCategories = !this.showAllCategories;
+    this.updateDisplayedCategories();
+  }
 }
-
